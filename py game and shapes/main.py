73d16ada54070f38806097148431410c7abaced6
benfred123321 <benjamin.lyes@gmail.com>
2024-10-22 18:58:12 +0100

    Add files via upload

---
+++ py game and shapes/main.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init()
+
+#creating screen
+screen= pygame.display.set_mode((800,800))
+
+#adding title to this game window
+pygame.display.set_caption("pygame and shapes")
+
+#adding backround coulor to your screen
+screen.fill("sky blue")
+
+pygame.display.update()
+
+class Circle():
+    def __init__(self):
+        self.radius= 40
+        self.
+
+while True:
+    
+    screen.fill("sky blue")
+    pygame.display.update()
